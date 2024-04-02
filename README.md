ecd3e965f8189999a2bcdb8d2a04d0af243e374d
Yuyang Qii <972563648@qq.com>
2024-04-01 23:04:21 -0700

    init

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # TECHIN510 Lab1
-
+A personal website for TECHIN510 Lab 1
 
 ## How to run
+```bash
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
-streamlit run app.py
 
-
+## What's included
+- `app.py`: The main Streamlit application script
+- `requirements.txt`: The list of Python dependencies for the project
+- `.gitignore`: Specifies untracked files to ignore
 
 ## Lessons Learned
+-  Building simple personal website with Streamlit
+-  Managing Python dependencies with `requirements.txt`
+- Use .gitignore to efficiently upload Python projects to GitHub
 
 ## Questions
+- How do I do column layout and custom margins in Streamlit, such as image spacing or line spacing for website text?
--- app.py
+++ app.py
@@ -1,25 +1,62 @@
 
 import streamlit as st
 
-st.title("👋Hi, my name is yuyang")
+st.title("👋 Hi, my name is Yuyang Qi")
 
 col1, col2 = st.columns(2)
 
 with col1:
-   st.subheader("👩‍🎨About me")
+   st.subheader("👩‍🎨 About me")
    st.markdown("""
-    - School:  University of Wahington  &  Tsinghua University
-    - Major:  Connected Devices  &  Landscape Architecture      
+    - 🎓 Education:  
+       University of  Wahington   
+       Tsinghua University
+    - 📚 Major:  
+       Connected Devices 
+       & Landscape Architecture      
     """)
-   st.subheader("💡Skills")
+   st.subheader("💡 Skills")
    st.markdown("""
     - UX Design
-    - UX Research   
+    - UX Research
+    - Prototyping   
     """)
+   st.subheader("🤩 Interests")
+   st.markdown("""
+    - Photography
+    - Handicrafts
+    - Painting  
+    - xing'haoSleeping 😴           
+    """)
+
 
 with col2:
    image_path = 'images/photo.png'
-   st.image(image_path, caption='Mountain Rainier')
+   st.image(image_path, caption='at Mountain Rainier🗻')
 
 
-st.subheader("I enjoy  painting, particularly watercolors. I also have a passion for photography and doing crafts, which enables me to explore and express my creativity from various perspectives.")
+st.markdown("I enjoy  painting, particularly watercolors. I also have a passion for photography and doing crafts, which enables me to explore and express my creativity from various perspectives.")
+
+col1, col2 = st.columns(2)
+with col1:
+    image_path = 'images/photo2.png'
+    st.image(image_path, caption='in Chongqing 🌆')
+
+with col2:
+    image_path = 'images/photo3.png'
+    st.image(image_path, caption='LA 🌇')  
+
+col1, col2 = st.columns(2)
+with col1:
+    image_path = 'images/panda.png'
+    st.image(image_path, caption='Handmade Panda😍')
+
+with col2:
+    image_path = 'images/bunny.png'
+    st.image(image_path, caption='Handmade Bunny🐇')  
+
+image_path = 'images/painting.png'
+st.image(image_path, caption='dramatis personae🎭')  
+
+image_path = 'images/watercolor.png'
+st.image(image_path, caption='classical gardens🏡')
